0692b826c5943de8d1c934031d6b88eee9187b66
hieunba <hi3unb@gmail.com>
2018-07-20 10:37:22 +0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# windows_update
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,20 @@
+name 'windows_update'
+maintainer 'Nghiem Ba Hieu'
+maintainer_email 'hi3unb@gmail.com'
+license 'Apache-2.0'
+description 'Installs/Configures windows_update'
+long_description 'Installs/Configures windows_update'
+version '0.1.0'
+chef_version '>= 12.14' if respond_to?(:chef_version)
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/windows_update/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/windows_update'
